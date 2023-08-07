5c4e3808ef8e2045c261f84ef6a4f562e169c621
Mr.Sj <42267783+sj0510@users.noreply.github.com>
2023-08-07 12:43:08 +0530

    Update MySdk.kt

--- mylibrarysdk/src/main/java/com/sj/mylibrarysdk/MySdk.kt
+++ mylibrarysdk/src/main/java/com/sj/mylibrarysdk/MySdk.kt
@@ -3,8 +3,9 @@ package com.sj.mylibrarysdk
 import android.content.Context
 import android.widget.Toast
 
-object MySdk {
-    fun showToast(context: Context, message: String) {
+public class ToastSDK {
+    public fun showToast(context: Context, message: String){
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
+
 }
